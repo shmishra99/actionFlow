e6bc6c7400c3071351fc9f0f939eb4572d6dc12d
Shivam Mishra <shivammishr@google.com>
2023-03-16 21:23:22 +0530

    action workflow for priority change

--- .github/scripts/issue_priority.js
+++ .github/scripts/issue_priority.js
@@ -1,4 +1,4 @@
-const CONSTENT_VALUES = require('./constant')
+// const CONSTENT_VALUES = require('./constant')
 
 /* 
 When author of the issue/pr comment than
@@ -9,31 +9,45 @@ module.exports = async ({ github, context }) => {
     console.log('entered unmark for issueid = ', context.payload.issue.number);
     console.log('issue author is ', context.payload.issue.user.login);
     console.log('issue latest commenter is ', context.payload.sender.login);
-  
-    if (context.payload.issue.state !== CONSTENT_VALUES.GLOBALS.STATE.CLOSED &&
-        context.payload.issue.user.login == context.payload.sender.login) {
-        for (const label of context.payload.issue.labels) {
 
-            if (label.name.includes(CONSTENT_VALUES.GLOBALS.LABELS.STALE)) {
-                console.log("Removing label: " + CONSTENT_VALUES.GLOBALS.LABELS.STALE)
-                await github.rest.issues.removeLabel({
-                    issue_number: context.issue.number,
-                    owner: context.repo.owner,
-                    repo: context.repo.repo,
-                    name: CONSTENT_VALUES.GLOBALS.LABELS.STALE
 
-                })
-            }
-            if (label.name.includes(CONSTENT_VALUES.GLOBALS.LABELS.AWAITINGRES)) {
-                console.log("Removing label : " + CONSTENT_VALUES.GLOBALS.LABELS.AWAITINGRES)
-                await github.rest.issues.removeLabel({
-                    issue_number: context.issue.number,
-                    owner: context.repo.owner,
-                    repo: context.repo.repo,
-                    name: CONSTENT_VALUES.GLOBALS.LABELS.AWAITINGRES
+   let resp =  await issues.listEventsForTimeline({
+    owner: context.repo.owner,
+        repo:context.repo.repo,
+        issue_number:1,
+    });
 
-                })
-            }
-        }
-    }
+    console.log(JSON.stringify(resp))
+
+
+    // if (context.payload.issue.state !== CONSTENT_VALUES.GLOBALS.STATE.CLOSED &&
+    //     context.payload.issue.user.login == context.payload.sender.login) {
+
+
+
+
+    //     for (const label of context.payload.issue.labels) {
+
+    //         if (label.name.includes(CONSTENT_VALUES.GLOBALS.LABELS.STALE)) {
+    //             console.log("Removing label: " + CONSTENT_VALUES.GLOBALS.LABELS.STALE)
+    //             await github.rest.issues.removeLabel({
+    //                 issue_number: context.issue.number,
+    //                 owner: context.repo.owner,
+    //                 repo: context.repo.repo,
+    //                 name: CONSTENT_VALUES.GLOBALS.LABELS.STALE
+
+    //             })
+    //         }
+    //         if (label.name.includes(CONSTENT_VALUES.GLOBALS.LABELS.AWAITINGRES)) {
+    //             console.log("Removing label : " + CONSTENT_VALUES.GLOBALS.LABELS.AWAITINGRES)
+    //             await github.rest.issues.removeLabel({
+    //                 issue_number: context.issue.number,
+    //                 owner: context.repo.owner,
+    //                 repo: context.repo.repo,
+    //                 name: CONSTENT_VALUES.GLOBALS.LABELS.AWAITINGRES
+
+    //             })
+    //         }
+    //     }
+    // }
 }
