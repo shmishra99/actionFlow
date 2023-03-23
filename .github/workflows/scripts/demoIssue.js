59e294d53eee7965f851f55fa9adcc109bf02073
Shivam Mishra <shivammishr@google.com>
2023-03-23 14:41:58 +0530

    added file to change the issue label from p0 to p1 if more then 60 days old.

--- .github/workflows/scripts/demoIssue.js
+++ .github/workflows/scripts/demoIssue.js
@@ -14,12 +14,12 @@ module.exports = async ({ github, context }) => {
     // });
 
     for(let i=0;i<2;i++){
-        
+        let tit = i + " issue"
         github.rest.issues.create({
         owner: context.repo.owner,
         repo: context.repo.repo,
-        title:i + " issue",
-        labels:"stat:awaiting response"
+        title: tit,
+        labels:["stat:awaiting response"]
       });
  
     }
