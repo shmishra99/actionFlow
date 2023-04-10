39579fc2080c47a5b411ebb847b0452026a3882e
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-04-10 15:31:05 +0530

    Update staleCSAT.js

--- .github/workflows/scripts/staleCSAT.js
+++ .github/workflows/scripts/staleCSAT.js
@@ -1,15 +1,15 @@
 
 const csat = require('./CSAT.js');
-const constant = require("./constant.js")
+const constant = require("./constant.js");
 module.exports = async ({ github, context }) => {
 
   let date = new Date();
   let totalMilliSeconds = date.getTime();
-  let minutes = 20
+  let minutes = 20;
   let millisecondsToSubtract = minutes * 60 * 1000;
-  let closeTime = totalMilliSeconds-millisecondsToSubtract
-  let newDate = new Date(closeTime)
-  let ISOCloseTime = newDate.toISOString() 
+  let closeTime = totalMilliSeconds-millisecondsToSubtract;
+  let newDate = new Date(closeTime);
+  let ISOCloseTime = newDate.toISOString();
   let closeTimeIssues  = await github.rest.issues.listForRepo({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -17,29 +17,26 @@ module.exports = async ({ github, context }) => {
     labels:"stale",
     since:ISOCloseTime
   });
-
- let ISSUESLIST = closeTimeIssues.data
+ let ISSUESLIST = closeTimeIssues.data;
  for(let i=0;i<ISSUESLIST.length;i++){ 
   if(ISSUESLIST[i].node_id && ISSUESLIST[i].node_id.indexOf("PR") !=-1)
-     continue
+     continue;
    
   let comments = await github.rest.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: ISSUESLIST[i].number
   });
-  
-  
   let noOfComments = comments.data.length;
-  let lastComment = comments.data[noOfComments-1]
-  let strCom = JSON.stringify(lastComment)
+  let lastComment = comments.data[noOfComments-1];
+  let strCom = JSON.stringify(lastComment);
   if(strCom.indexOf(constant.MODULE.CSAT.MSG) == -1){
        context.payload.issue = {}
        context.payload.issue.number = ISSUESLIST[i].number
        context.payload.issue.labels = ISSUESLIST[i].labels
        context.payload.issue.html_url = ISSUESLIST[i].html_url
       
-       csat({github, context})
+       csat({github, context});
   }
  }
 }
