f114392f784ce1c7092b4b9d1cc250dd8549b07e
“shmishra99” <shivammishr@google.com>
2023-05-23 16:26:47 +0530

    issue

--- .github/workflows/scripts/stale-close-when-update.js
+++ .github/workflows/scripts/stale-close-when-update.js
@@ -1,10 +1,14 @@
 
 const CONSTENT_VALUES = require('./constant');
+
 /*
-Invoked from staleCSAT.js and CSAT.yaml file to 
-post survey link in closed issue.
+   Script will close all issues that have been labeled as stale for more than 7 days
+   and have had at comment since being labeled, invoked from stale management.
 */
+
 module.exports = async ({ github, context }) => {
+
+    // fetch all the issues with stale label.
     let issues = await github.rest.issues.listForRepo({
         owner: context.repo.owner,
         repo: context.repo.repo,
@@ -19,6 +23,8 @@ module.exports = async ({ github, context }) => {
         let number = issueList[i].number;
         let nodeType = issueList[i].node_id
         console.log("issue list",issueList[i])
+       
+        //fetch all the events inside the issue.
         let resp = await github.rest.issues.listEventsForTimeline({
             owner: context.repo.owner,
             repo: context.repo.repo,
@@ -28,29 +34,28 @@ module.exports = async ({ github, context }) => {
         let events = resp.data;
         let closeIssue = false
         for (let i = 0; i < events.length; i++) {
-            let event_details = events[i];      
+            let event_details = events[i];  
+            // Check if issue is marked as stale.    
             if (event_details.event == 'labeled' && event_details.label && event_details.label.name == "stale") {  
                 let currentDate = new Date();
                 let labeledDate = new Date(event_details.created_at)
                 let timeInDays = (currentDate - labeledDate) / 86400000
                 console.log(`Issue ${number} stale label is ${timeInDays} days old.`)
                 let closeAfterStale = 7
-               
+
                 if(nodeType.startsWith('PR'))
                    closeAfterStale = 14
-
+   
                 if (timeInDays > closeAfterStale)
                     closeIssue = true
-                
-                console.log("line 45 " + nodeType + "  " + closeAfterStale)
             }
             if (event_details.event == 'unlabeled' && event_details.label && event_details.label.name == "stale"){
-                console.log(`Stale is unlabel for issue ${number}`)
+                console.log(`Stale is unlabel for issue ${number}.`)
                     closeIssue = false
             }
         }
         if(closeIssue){
-            console.log(`Closing the issue ${number} more then 7 days old with stale label`)
+            console.log(`Closing the issue ${number} more then 7 days old with stale label.`)
             await github.rest.issues.update({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
