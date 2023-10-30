a560438a35c2bba12219af3ff2b655230640a1ee
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-10-30 12:45:47 +0530

    Update auto-assignment.js

--- .github/workflows/scripts/auto-assignment.js
+++ .github/workflows/scripts/auto-assignment.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github, context }) =>  {
     console.log("line 3", context.payload)
-    const issueNumber = context.payload.number;
-    
+    // Check if it is a then return issue number else return PR number
+    const issueNumber = context.payload.issue ? context.payload.issue.number : context.payload.number    
     const assigneesList = ['shmishra99','sushreebarsa'];
     console.log("assignee list",assigneesList)
     console.log("entered auto assignment for this issue:  ", issueNumber);
