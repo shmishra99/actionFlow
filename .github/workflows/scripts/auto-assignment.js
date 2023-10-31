b6787dfa16ebc7f1046864089ae72df8b682ca66
Shivam Mishra <124146945+shmishra99@users.noreply.github.com>
2023-10-31 18:02:12 +0530

    Update auto-assignment.js

--- .github/workflows/scripts/auto-assignment.js
+++ .github/workflows/scripts/auto-assignment.js
@@ -1,35 +1,60 @@
-module.exports = async ({ github, context }) =>  {
-    console.log("line 3", context.payload)
-    // Check if it is a then return issue number else return PR number
-    // const issueNumber = context.payload.issue ? context.payload.issue.number : context.payload.number    
-    let issueNumber;
-    let assigneesList; 
-    //if issue event trigger then it will run this. 
-    if(context.payload.issue){
-        assigneesList = ['shmishra99','sushreebarsa']
-        issueNumber =  context.payload.issue.number
-    }
-    else {
-         assigneesList = ['ckulkarni97']
-         issueNumber = context.payload.number 
-    }
-    console.log("assignee list",assigneesList)
-    console.log("entered auto assignment for this issue:  ", issueNumber);
-    if (!assigneesList.length) {
-      console.log('No assignees found for this repo.');
-      return;
-    }
-    let noOfAssignees = assigneesList.length;
-    let selection = issueNumber % noOfAssignees;
-    let assigneeForIssue = assigneesList[selection]
+/**
+ * @license
+ * Copyright 2023 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
 
-      console.log(
-        'issue Number =', issueNumber + ', assigning issue to:',
-        assigneeForIssue);
-      return github.rest.issues.addAssignees({
-        issue_number: context.issue.number,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        assignees:[assigneeForIssue] 
-    });
+/** Automatically assign issues and PRs to users in the `assigneesList` 
+ *  on a rotating basis.
+
+  @param {!object}
+    GitHub objects can call GitHub APIs using their built-in library functions.
+    The context object contains issue and PR details.
+*/
+
+module.exports = async ({ github, context }) => {
+  let issueNumber;
+  let assigneesList;
+  // Is this an issue? If so, assign the issue number. Otherwise, assign the PR number.
+  if (context.payload.issue) {
+    //assignee List for issues. 
+    assigneesList = ["tilakrayal"];
+    issueNumber = context.payload.issue.number;
+  } else {
+    //assignee List for PRs. 
+    assigneesList = ['shmishra99','sushreebarsa','ckulkarni97']
+    issueNumber = context.payload.number;
   }
+  console.log("assignee list", assigneesList);
+  console.log("entered auto assignment for this issue:  ", issueNumber);
+  if (!assigneesList.length) {
+    console.log("No assignees found for this repo.");
+    return;
+  }
+  let noOfAssignees = assigneesList.length;
+  let selection = issueNumber % noOfAssignees;
+  let assigneeForIssue = assigneesList[selection];
+
+  console.log(
+    "issue Number = ",
+    issueNumber + " , assigning to: ",
+    assigneeForIssue
+  );
+  return github.rest.issues.addAssignees({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    assignees: [assigneeForIssue],
+  });
+};
